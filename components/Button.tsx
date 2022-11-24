b3bf9a6ab47ce59b15fee784ae728c561d85e8ea
Niels Bik <nielsrowinbik@gmail.com>
2022-11-24 15:44:11 +0100

    Remove unused components

--- components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-const Button = ({ children, href }) => (
-  <Link href={href} passHref>
-    <a className="bg-red-200 px-4 py-3 no-underline">{children}</a>
-  </Link>
-);
-
-export default Button;
--- components/ProseSection.tsx
+++
@@ -1,5 +0,0 @@
-const ProseSection = ({ children }) => (
-  <section className="prose prose-2xl mx-auto">{children}</section>
-);
-
-export default ProseSection;
